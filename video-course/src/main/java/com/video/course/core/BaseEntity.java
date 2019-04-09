171d14a4a620a9b9f98c00fc37ac3c14fc5858bf
MPH\G521896 <sahil.arora@idemia.com>
2019-04-09 20:59:34 +0530

    first commit - project structure and BaseEntity

---
+++ video-course/src/main/java/com/video/course/core/BaseEntity.java
@@ -0,0 +1,48 @@
+package com.video.course.core;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.util.Calendar;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(updatable = false, name = "creation_time_stamp")
+    @CreationTimestamp
+    private Calendar creationTimeStamp;
+
+    @Column(name = "updation_time_stamp")
+    @UpdateTimestamp
+    private Calendar updationTimeStamp;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Calendar getCreationTimeStamp() {
+        return creationTimeStamp;
+    }
+
+    public void setCreationTimeStamp(Calendar creationTimeStamp) {
+        this.creationTimeStamp = creationTimeStamp;
+    }
+
+    public Calendar getUpdationTimeStamp() {
+        return updationTimeStamp;
+    }
+
+    public void setUpdationTimeStamp(Calendar updationTimeStamp) {
+        this.updationTimeStamp = updationTimeStamp;
+    }
+}
